e8e5a44b4332e58894a28e6583d8866a66d55cc5
MarkSzakal <57964946+MarkSzakal@users.noreply.github.com>
2024-10-16 21:31:21 -0400

    Update musicPlayer.java

--- src/musicPlayer.java
+++ src/musicPlayer.java
@@ -1,23 +1,23 @@
 import components.queue.Queue;
 import components.queue.Queue1L;
 
-/** */
 public class musicPlayer {
-    /** */
 
     private Queue<String> playlist;
+    private String currentTrack;
+    private boolean isPlaying;
 
-    /** */
     public musicPlayer() {
         this.playlist = new Queue1L<>();
+        this.currentTrack = null;
+        this.isPlaying = false;
     }
 
-    /** */
-    public void queue(String song) {
+    public void addSong(String song) {
         this.playlist.enqueue(song);
     }
 
-    public void remove(String song) {
+    public void removeSong(String song) {
         Queue<String> temp = new Queue1L<>();
 
         while (this.playlist.length() > 0) {
@@ -30,8 +30,35 @@ public class musicPlayer {
         }
     }
 
+    public boolean play() {
+        boolean isPlaying = false;
+        if (this.playlist.length() > 0) {
+            isPlaying = true;
+        }
+        return isPlaying;
+    }
+
+    public boolean pause() {
+        if (this.isPlaying) {
+            this.isPlaying = false;
+        }
+        return this.isPlaying;
+    }
+
+    public String getTrack() {
+        return this.currentTrack;
+    }
+
     /** */
     public static void main(String[] args) {
+        musicPlayer player1 = new musicPlayer();
         String song = null;
+        player1.addSong("Heartless");
+        player1.removeSong("Father Stretch My Hands Pt. 1");
+        player1.getTrack();
+        player1.play();
+        player1.pause();
+        player1.removeSong("Heartless");
+        player1.play();
     }
 }
